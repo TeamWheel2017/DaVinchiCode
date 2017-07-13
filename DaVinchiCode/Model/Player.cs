fd96e23d0a063c76ebc4b3a2fe8e5c2ad7270410
HeekangPark <hkang33@naver.com>
2017-07-14 07:37:34 +0900

    DaVinchiCode : (Uncompilable) 	Added : 		class HandCard 	Modified : 		class Player - XML 주석 추가
    	Memo : 현재 Model 부분 작성 중

--- DaVinchiCode/Model/Player.cs
+++ DaVinchiCode/Model/Player.cs
@@ -6,14 +6,54 @@ using System.Threading.Tasks;
 
 namespace DaVinchiCode.Model
 {
-    //플레이어 클래스
+	/// <summary>
+	/// Player의 hand의 Card들을 출력을 위한 클래스
+	/// </summary>
+	public class HandCard
+	{
+		public CardStatus Status { get; private set; }
+		public CardColor Color { get; private set; }
+
+		public bool? IsJokerCard { get; private set; }
+		public bool? IsNumCard { get { return !IsJokerCard; } }
+		public int? Num { get; private set; }
+
+		public HandCard(Card CardOnHand)
+		{
+			Status = CardOnHand.Status;
+			Color = CardOnHand.Color;
+			
+			//JokerCard, NumCard인지에 따라 다르게 초기화해야됨
+		}
+	}
+	
+	/// <summary>
+	/// 게임 참가 플레이어
+	/// </summary>
 	public class Player
 	{
+		/// <summary>
+		/// Player가 소속된 Game
+		/// </summary>
 		private Game gamecore;
-		List<Card> hand;
-		int? newCardIdx;
+		/// <summary>
+		/// Player가 들고 있는 Card
+		/// </summary>
+		private List<Card> hand;
+		/// <summary>
+		/// Game의 Ground로부터 새로 받은 Card의 hand에서의 Index를 저장하는 변수
+		/// </summary>
+		private int? newCardIdx;
 
-        //초기 설정
+		/// <summary>
+		/// Player의 Hand가 변경되었음을 알리는 이벤트
+		/// </summary>
+		public event EventHandler HandModifed;
+
+		/// <summary>
+		/// 생성자
+		/// </summary>
+		/// <param name="gamecore">Player가 소속된 Game</param>
 		public Player(Game gamecore)
 		{
 			this.gamecore = gamecore;
@@ -21,7 +61,11 @@ namespace DaVinchiCode.Model
 			hand = null;
 			newCardIdx = null;
 		}
-        //숫자카드일 경우 추가 함수
+
+		/// <summary>
+		/// 새 Card를 추가하는 메소드
+		/// </summary>
+		/// <param name="card">새로 받은 NumCard</param>
 		public void AddNewCard(NumCard card)
 		{
 			for(int i = 0; i < hand.Count; i++)
@@ -29,45 +73,61 @@ namespace DaVinchiCode.Model
 				if (hand[i] is NumCard)
 				{
 					NumCard curCard = hand[i] as NumCard;
-                    //새로 추가하려는 카드 바로 오른쪽에 놓일 카드 위치를 확인 후 해당 위치에 카드 추가
+					
+					//hand에서 Card는 순서대로 배열되어 있다.
+					//그러므로 현재 탐색 위치의 Card가 새로 받은 Card보다 큰 첫 번째 카드라면 여기에 새로 받은 Card가 들어가야 한다.
 					if(curCard.CompareTo(card) < 0)
 					{
-						hand.Insert(i, card);
-						break;//위치가 정해졌을 경우 break
+						hand.Insert(i, card); //hand에 새 Card를 추가한다.
+						card.Status = CardStatus.Hidden; //추가한 Card의 CardStatus를 Hidden으로 바꾼다.
+						newCardIdx = i; //newCardIdx에 추가한 위치를 기록해놓는다.
+						break;
 					}
 				}
 			}
 		}
-        //조커카드일 경우 추가 함수
+		/// <summary>
+		/// 새 Card를 추가하는 메소드
+		/// </summary>
+		/// <param name="card">새로 받은 JokerCard</param>
+		/// <exception cref="System.Exception">입력받은 handIdx가 유효하지 않으면 예외를 throw한다.</exception>
 		public void AddNewCard(JokerCard card, int handIdx)
 		{
-			if(handIdx > hand.Count)
-			{
-				throw new Exception("Invalid handIdx");
-			}
-
-			hand.Insert(handIdx, card);
-		}
-        //상대 카드 추측 함수
-		public bool GuessResult(int handIdx, Card guessCard)
-		{
-            //상대가 가진 카드 갯수 범위를 넘어가면 throw
+			//만약 입력받은 handIdx가 유효하지 않으면 예외를 throw한다.
 			if (handIdx > hand.Count)
 			{
 				throw new Exception("Invalid handIdx");
 			}
-            //추측한 카드가 맞는지 확인
-			if (hand[handIdx].Equals(guessCard))
+			
+			hand.Insert(handIdx, card); //요청한 위치에 Card를 넣는다.
+			card.Status = CardStatus.Hidden; //추가한 Card의 CardStatus를 Hidden으로 바꾼다.
+			newCardIdx = handIdx; //newCardIdx에 추가한 위치를 기록해놓는다.
+		}
+
+		/// <summary>
+		/// 다른 Player의 Guess문의에 답하는 메소드
+		/// </summary>
+		/// <param name="handIdx">문의한 Index</param>
+		/// <param name="guessCard">문의한 Card</param>
+		/// <returns>handIdx위치에 guessCard가 맞으면 true, 틀리면 false 반환</returns>
+		/// /// <exception cref="System.Exception">입력받은 handIdx가 유효하지 않으면 예외를 throw한다.</exception>
+		public bool GuessResult(int handIdx, Card guessCard)
+		{
+			//만약 입력받은 handIdx가 유효하지 않으면 예외를 throw한다.
+			if (handIdx > hand.Count)
 			{
-				return true;//맞으면 참 반환(???어차피 Equals함수 반환값이 bool이니깐 그냥 이렇게 안하고 반환값 받으면 안됨?)
+				throw new Exception("Invalid handIdx");
+			}
+
+			if(hand[handIdx].Equals(guessCard)) //만약 Guess한 Card가 맞으면
+			{
+				hand[handIdx].Status = CardStatus.Shown; //맞춘 Card를 깐다.
+				return true;
 			}
 
 			return false;
 		}
-        //덮어져있는 무작위 카드 획득 함수
-		public void TakeCard(int groundIdx)
-		{
-			
-		}
+
+		//자신의 hand를 보여주는 메소드 필요
 	}
 }
